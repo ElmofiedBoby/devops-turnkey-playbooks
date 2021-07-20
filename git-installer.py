479c4214bd315acc7d87749265c776ef0823eb3c
overseer <you@example.com>
2021-07-20 17:59:06 +0000

    updated playbooks

--- git-installer.py
+++ git-installer.py
@@ -1,12 +1,12 @@
-from caprover_api import caprover_api
+from caprover_api.caprover_api import caprover_api
 import importlib
 import sys
 
-importlib.reload(caprover_api)
+#importlib.reload(caprover_api)
 #Argument syntax: git-installer.py,app_name,url
 
-print(sys.argv[1])
-print(sys.argv[2])
+#print(sys.argv[1])
+#print(sys.argv[2])
 
 cap = caprover_api.CaproverAPI(
     dashboard_url=sys.argv[2],
