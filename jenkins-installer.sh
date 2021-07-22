618da4f6df324c4c602213217dad7a185b0d8ef7
Nithin Joseph <nithjoe1@gmail.com>
2021-07-22 12:33:51 -0400

    don't need git-configure here anymore

--- jenkins-installer.sh
+++ jenkins-installer.sh
@@ -3,9 +3,9 @@ ansible-playbook jenkins-install.yml
 echo "5s wait"
 sleep 5s
 ansible-playbook jenkins-ssh.yml
-echo "5s wait"
-sleep 5s
-ansible-playbook git-configure.yml
+#echo "5s wait"
+#sleep 5s
+#ansible-playbook git-configure.yml
 echo "15s wait"
 sleep 15s
 ansible-playbook jenkins-configure.yml
